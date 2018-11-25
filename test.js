917819e123277175e1a207f3e056d5a65706da19
Guilherme Afonso <guilherme.asp@live.com>
2018-11-25 20:42:56 -0300

    Finished and commented

--- test.js
+++ test.js
@@ -1,44 +1,31 @@
 const sat = require('sat_modelo.js')
-const {performance} = require('perf_hooks')
-console.log("------------------------------------------------------------------------")
-t0 = performance.now()
-result = sat.solve('./tests/tutorial.cnf')
-t1 = performance.now()
-console.log("tutorial: isSat = " + result.isSat + "  SatisfyingAssignment = " + result.satisfyingAssignment)
-console.log("Execution time: "+(t1-t0)+"ms")
-console.log("------------------------------------------------------------------------")
-
-t0 = performance.now()
-result = sat.solve('./tests/simple0.cnf')
-t1 = performance.now()
-console.log("simple0: isSat = " + result.isSat + "  SatisfyingAssignment = " + result.satisfyingAssignment)
-console.log("Execution time: "+(t1-t0)+"ms")
+const { performance } = require('perf_hooks')
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 console.log("------------------------------------------------------------------------")
 
-t0 = performance.now()
-result = sat.solve('./tests/simple1.cnf')
-t1 = performance.now()
-console.log("simple1: isSat = " + result.isSat + "  SatisfyingAssignment = " + result.satisfyingAssignment)
-console.log("Execution time: "+(t1-t0)+"ms")
-console.log("------------------------------------------------------------------------")
+rl.question("Enter the file name:", (fileName) => {
+    t0 = performance.now()
+    let result = sat.solve('./tests/' + fileName)
+    t1 = performance.now()
 
-t0 = performance.now()
-result = sat.solve('./tests/simple2.cnf')
-t1 = performance.now()
-console.log("simple2: isSat = " + result.isSat + "  SatisfyingAssignment = " + result.satisfyingAssignment)
-console.log("Execution time: "+(t1-t0)+"ms")
-console.log("------------------------------------------------------------------------")
+    if (result.isSat) {
+        console.log("File tested: "+fileName)
+        console.log("Is SAT!")
+        console.log("One satisfying sssignment is: |" + result.satisfyingAssignment.toString().replace(/,/g,"|")+ "|")
+        console.log("Execution time: " + (t1 - t0) + "ms")
+        console.log("------------------------------------------------------------------------")
+    } else if (result.satisfyingAssignment == 'SpecificationProblemFound') {
+        console.log("A specification problem was found in the" + fileName + "file!")
+        console.log("------------------------------------------------------------------------")
+    } else {
+        console.log("File tested: "+fileName)
+        console.log("Is not SAT!")
+        console.log("------------------------------------------------------------------------")
+    }
+    rl.close()
+})
 
-t0 = performance.now()
-result = sat.solve('./tests/hole4.cnf')
-t1 = performance.now()
-console.log("hole4: isSat = " + result.isSat + "  SatisfyingAssignment = " + result.satisfyingAssignment)
-console.log("Execution time: "+(t1-t0)+"ms")
-console.log("------------------------------------------------------------------------")
-
-t0 = performance.now()
-result = sat.solve('./tests/hole5.cnf')
-t1 = performance.now()
-console.log("hole5: isSat = " + result.isSat + "  SatisfyingAssignment = " + result.satisfyingAssignment)
-console.log("Execution time: "+(t1-t0)+"ms")
-console.log("------------------------------------------------------------------------")
