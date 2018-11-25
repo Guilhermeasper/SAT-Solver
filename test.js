96e1ea410f48b3d5a5a04ed71b7814b83538be4a
Guilherme Afonso <guilherme.asp@live.com>
2018-11-25 20:45:57 -0300

    Finished and commented

--- test.js
+++ test.js
@@ -13,17 +13,18 @@ rl.question("Enter the file name:", (fileName) => {
     t1 = performance.now()
 
     if (result.isSat) {
-        console.log("File tested: "+fileName)
+        console.log("File tested: " + fileName)
         console.log("Is SAT!")
-        console.log("One satisfying sssignment is: |" + result.satisfyingAssignment.toString().replace(/,/g,"|")+ "|")
+        console.log("One satisfying sssignment is: |" + result.satisfyingAssignment.toString().replace(/,/g, "|") + "|")
         console.log("Execution time: " + (t1 - t0) + "ms")
         console.log("------------------------------------------------------------------------")
     } else if (result.satisfyingAssignment == 'SpecificationProblemFound') {
         console.log("A specification problem was found in the" + fileName + "file!")
         console.log("------------------------------------------------------------------------")
     } else {
-        console.log("File tested: "+fileName)
+        console.log("File tested: " + fileName)
         console.log("Is not SAT!")
+        console.log("Execution time: " + (t1 - t0) + "ms")
         console.log("------------------------------------------------------------------------")
     }
     rl.close()
